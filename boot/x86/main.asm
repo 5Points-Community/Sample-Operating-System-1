37cced9cfea1deac5423c2e349c4fd121dac71aa
5Points-Community <lordxarmado69@hotmail.com>
2024-05-31 10:32:16 -0300

    Update main.asm

--- boot/x86/main.asm
+++ boot/x86/main.asm
@@ -4,18 +4,18 @@ org 0x7C00
 ; Defina quantos Bits é o Modo Real
 bits 16
 
-; Cria as Variáveis
-section .data
-	; Defina o quanto de RAM pode ser Usada no Máximo
-	max_ram db 0x0040
- 	; Defina o quanto de RAM em Uso Atual
- 	use_ram db 0x0000
-
 ; Código Geral
 ; Simplesmente um Loop Infinito
 loop:
 	jmp loop
 
-times 510-($-$$) db 0x0000
-db 0x0055
-db 0x00AA
+; Cria as Variáveis
+section .data
+	; Defina o quanto de RAM pode ser Usada no Máximo
+	max_ram dw 0x0040
+ 	; Defina o quanto de RAM em Uso Atual
+ 	use_ram dw 0x0000
+
+times 510-($-$$) db 0x00
+db 0x55
+db 0xAA
