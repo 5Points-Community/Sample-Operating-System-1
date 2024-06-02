6f60244d7e4d81c69d7c5dd3589d7c088ae463fb
5Points-Community <lordxarmado69@hotmail.com>
2024-06-02 20:20:28 -0300

    Update main.asm

--- boot/x86/main.asm
+++ boot/x86/main.asm
@@ -1,20 +1,25 @@
 ; Inicia o Código no Endereço de Boot
 org 0x7C00
 
-; Defina quantos Bits é o Modo Real
-bits 16
-
 ; Código Geral
 ; Simplesmente um Loop Infinito
 loop:
 	jmp loop
 
+; Aqui tem a Função de Abortar o Boot
+abort:
+	mov si, (ram_error)
+	mov 0x0E
+	int 0x10
+
 ; Cria as Variáveis
 section .data
 	; Defina o quanto de RAM pode ser Usada no Máximo
-	max_ram dw 0x0040
+	max_ram   db 0x800
  	; Defina o quanto de RAM em Uso Atual
- 	use_ram dw 0x0000
+ 	use_ram   db 0x000
+	; Mensagem de Aborto por excesso de RAM usada no Boot
+	ram_error db "Detectamos um Uso Escessivo de RAM no Bootloader, Abortamos o Boot por Segurança", 0x00
 
 times 510-($-$$) db 0x00
 db 0x55
