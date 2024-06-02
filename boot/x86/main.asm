a51c3bff88a615933199ead2ab3203061f77a947
5Points-Community <lordxarmado69@hotmail.com>
2024-06-02 20:52:41 -0300

    Update main.asm

--- boot/x86/main.asm
+++ boot/x86/main.asm
@@ -4,11 +4,11 @@ org 0x7C00
 ; Cria as Variáveis
 section .data
 	; Defina o quanto de RAM pode ser Usada no Máximo
-	max_ram   db 0x800
+	max_ram    db 0x800
  	; Defina o quanto de RAM em Uso Atual
- 	use_ram   db 0x000
+ 	use_ram    db 0x000
 	; Mensagem de Aborto por excesso de RAM usada no Boot
-	ram_error db "Detectamos um Uso Escessivo de RAM no Bootloader, Abortamos o Boot por Segurança", 0x00
+	ram_error  db "Detectamos um Uso Escessivo de RAM no Bootloader, Abortamos o Boot por Segurança", 0x00
 
 ; Código Geral
 section .text
@@ -20,20 +20,21 @@ loop:
 
 ; Aqui tem a Função de Abortar o Boot
 abort:
-	mov si, (ram_error)
+	mov  si, (ram_error)
+	call print_str
 	hlt
 
 ; Imprime a String... ou Algo do Tipo
 print_str:
-	mov ah, 0x0E
+	mov  ah, 0x0E
 
 ; Imprime o Caractere, EBAAAA
 print_chr:
 	lodsb
-	cmp al, 0x00
-	je  print_done
-	int 0x10
-	jmp print_chr
+	cmp  al, 0x00
+	je   print_done
+	int  0x10
+	jmp  print_chr
 
 ; Nome Auto-Explicativo, Certo?
 print_done:
